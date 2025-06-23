b3150372fb2a3f469e3e627e19dc6ada2ea8cdb0
KashishSutramSolution <kahsish.darji@sutramsolutions.com>
2025-06-23 12:28:21 +0530

    user_model

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,11 +14,29 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Api Demo'),
+        centerTitle: true,
+        backgroundColor:Colors.blue,
       ),
+
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          
+             Center(
+            child: ElevatedButton(
+              autofocus: true,
+             style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              ),
+              onPressed: (){},
+             child: Text("want to see the api data? Then press this",
+                 style: TextStyle(
+                  color: Colors.black
+                 ),),),
+             )
         ],
       ),
     );
